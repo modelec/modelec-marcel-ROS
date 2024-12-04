88806be5733c4ffc87a2435a1bcb9cd1510f1e2a
acki <ackimixs@protonmail.com>
2024-12-04 18:01:40 -0500

    refactor solenoid controller to use service for adding solenoids

--- src/modelec/include/modelec/solenoid_controller.hpp
+++ src/modelec/include/modelec/solenoid_controller.hpp
@@ -4,6 +4,7 @@
 #include <unordered_set>
 #include <wiringPi.h>
 #include <modelec_interface/msg/solenoid.hpp>
+#include <modelec_interface/srv/new_solenoid.hpp>
 
 namespace Modelec {
     class SolenoidController : public rclcpp::Node {
@@ -14,10 +15,10 @@ namespace Modelec {
 
         rclcpp::Subscription<modelec_interface::msg::Solenoid>::SharedPtr solenoid_subscriber_;
 
-        rclcpp::Subscription<modelec_interface::msg::Solenoid>::SharedPtr solenoid_add_subscriber_;
+        rclcpp::Service<modelec_interface::srv::NewSolenoid>::SharedPtr add_solenoid_service_;
 
         void activateSolenoid(const modelec_interface::msg::Solenoid::SharedPtr msg);
 
-        void addSolenoidPin(const modelec_interface::msg::Solenoid::SharedPtr msg);
+        void addSolenoid(const std::shared_ptr<modelec_interface::srv::NewSolenoid::Request> request, std::shared_ptr<modelec_interface::srv::NewSolenoid::Response> response);
     };
 }
--- src/modelec/src/solenoid_controller.cpp
+++ src/modelec/src/solenoid_controller.cpp
@@ -8,8 +8,8 @@ namespace Modelec {
         solenoid_subscriber_ = this->create_subscription<modelec_interface::msg::Solenoid>(
             "solenoid", 10, std::bind(&SolenoidController::activateSolenoid, this, std::placeholders::_1));
 
-        solenoid_add_subscriber_ = this->create_subscription<modelec_interface::msg::Solenoid>(
-            "add_solenoid", 10, std::bind(&SolenoidController::addSolenoidPin, this, std::placeholders::_1));
+        add_solenoid_service_ = this->create_service<modelec_interface::srv::NewSolenoid>(
+            "add_solenoid", std::bind(&SolenoidController::addSolenoid, this, std::placeholders::_1, std::placeholders::_2));
     }
 
     void SolenoidController::activateSolenoid(const modelec_interface::msg::Solenoid::SharedPtr msg) {
@@ -19,9 +19,18 @@ namespace Modelec {
         }
     }
 
-    void SolenoidController::addSolenoidPin(const modelec_interface::msg::Solenoid::SharedPtr msg) {
-        solenoid_pin_.insert(msg->pin);
-        pinMode(msg->pin, OUTPUT);
+    void SolenoidController::addSolenoid(const std::shared_ptr<modelec_interface::srv::NewSolenoid::Request> request,
+                                         std::shared_ptr<modelec_interface::srv::NewSolenoid::Response> response) {
+        if (solenoid_pin_.find(request->pin) != solenoid_pin_.end()) {
+            response->success = false;
+            return;
+        }
+
+        pinMode(request->pin, OUTPUT);
+        digitalWrite(request->pin, LOW);
+        solenoid_pin_.insert(request->pin);
+
+        response->success = true;
     }
 }
 

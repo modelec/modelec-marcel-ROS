e61088fb07cc6d01f37df07d859372e73ae23fe0
acki <ackimixs@protonmail.com>
2024-12-18 10:53:05 -0500

    Add LidarController implementation and update .gitignore

---
+++ src/modelec/src/lidar_controller.cpp
@@ -0,0 +1,22 @@
+#include <modelec/lidar_controller.hpp>
+
+namespace Modelec {
+    LidarController::LidarController() : Node("lidar_controller") {
+        subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "scan", 10, [this](const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+                processLidarData(msg);
+            });
+    }
+
+    void LidarController::processLidarData(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        // Process the received LiDAR data
+        RCLCPP_INFO(this->get_logger(), "Received LiDAR data: %s", msg->header.frame_id.c_str());
+    }
+}
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Modelec::LidarController>());
+    rclcpp::shutdown();
+    return 0;
+}

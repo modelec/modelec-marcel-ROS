a1262b1d0f8b570be260a61ef781db7801b17028
acki <ackimixs@protonmail.com>
2025-05-14 13:22:32 -0400

    service for modelec rasp pi

---
+++ start_ros2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # quitte en cas d'erreur
+
+source /opt/ros/jazzy/setup.bash
+source /home/modelec/modelec-serge-ROS/install/setup.bash
+
+export RCL_LOG_LEVEL=info
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/modelec-serge-ROS/fastdds_setup.xml
+export ROS_DOMAIN_ID=128
+
+# Lance ton launch file
+exec ros2 launch modelec_core test.modelec.launch.py

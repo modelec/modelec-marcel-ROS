6c511b320544fe3592e9d078831a773946c8ae04
acki <ackimixs@protonmail.com>
2025-05-20 16:17:11 -0400

    update install script

--- installl.sh
+++ installl.sh
@@ -15,13 +15,11 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update && sudo apt install ros-dev-tools -y
-
 sudo apt update
 
 sudo apt upgrade
 
-sudo apt install ros-jazzy-desktop -y
+sudo apt install ros-dev-tools ros-jazzy-desktop -y
 
 git submodule init
 git submodule update

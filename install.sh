6c4b96a62bd497523fbd063f6cd3c5ab8ca4ed85
acki <ackimixs@protonmail.com>
2025-05-20 16:57:58 -0400

    update install

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ locale  # verify settings
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
-sudo apt update && sudo apt install curl -y
+sudo apt update -y && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update
+sudo apt update -y
 
-sudo apt upgrade
+sudo apt upgrade -y
 
 sudo apt install ros-dev-tools ros-jazzy-desktop -y
 

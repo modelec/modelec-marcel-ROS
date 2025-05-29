c2a24553ce680c6e993641946a4bb6fb4ee9e2f8
acki <ackimixs@protonmail.com>
2025-05-29 08:32:05 -0400

    float

--- src/modelec_strat/src/enemy_manager.cpp
+++ src/modelec_strat/src/enemy_manager.cpp
@@ -222,7 +222,7 @@ namespace Modelec
                 last_publish_time_ = this->now();
                 last_movement_time_ = this->now(); // Mise à jour du dernier vrai mouvement
                 enemy_pos_pub_->publish(enemy_pos);
-                RCLCPP_INFO(this->get_logger(), "Enemy moved: x=%d, y=%d", enemy_pos.x, enemy_pos.y);
+                RCLCPP_INFO(this->get_logger(), "Enemy moved: x=%f, y=%f", enemy_pos.x, enemy_pos.y);
             }
             else
             {
@@ -230,7 +230,7 @@ namespace Modelec
                 if ((now - last_movement_time_).seconds() > max_stationary_time_s_)
                 {
                     enemy_long_time_pub_->publish(last_enemy_pos_);
-                    RCLCPP_WARN(this->get_logger(), "Enemy has been stationary for too long at x=%d y=%d",
+                    RCLCPP_WARN(this->get_logger(), "Enemy has been stationary for too long at x=%f y=%f",
                                 last_enemy_pos_.x, last_enemy_pos_.y);
 
                     last_movement_time_ = now;

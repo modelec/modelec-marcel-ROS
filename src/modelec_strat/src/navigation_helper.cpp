57273c153466cabff1bccaa57ef527d425b61e75
acki <ackimixs@protonmail.com>
2025-05-29 16:22:59 -0400

    log

--- src/modelec_strat/src/navigation_helper.cpp
+++ src/modelec_strat/src/navigation_helper.cpp
@@ -131,12 +131,12 @@ namespace Modelec
 
     void NavigationHelper::Update()
     {
-        if ((node_->now() - last_odo_get_pos_time_).seconds() > 0.5)
+        /*if ((node_->now() - last_odo_get_pos_time_).seconds() > 0.5)
         {
             std_msgs::msg::Empty empty_msg;
             odo_get_pos_pub_->publish(empty_msg);
             last_odo_get_pos_time_ = node_->now();
-        }
+        }*/
     }
 
     void NavigationHelper::SendGoTo()

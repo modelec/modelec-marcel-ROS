97564e258e2347efba654406d13b855d9e117b16
acki <ackimixs@protonmail.com>
2025-05-29 06:07:27 -0400

    install script

--- start_ros2.sh
+++ start_ros2.sh
@@ -2,11 +2,11 @@
 set -e
 
 source /opt/ros/jazzy/setup.bash
-source /home/modelec/modelec-serge-ROS/install/setup.bash
+source /home/modelec/modelec-marcel-ROS/install/setup.bash
 
 export RCL_LOG_LEVEL=info
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/modelec-serge-ROS/fastdds_setup.xml
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/modelec-marcel-ROS/fastdds_setup.xml
 export ROS_DOMAIN_ID=128
 
 exec ros2 launch modelec_core modelec.launch.py

7a9efb30091ca114862d4be44eb6de627456b2e6
acki <ackimixs@protonmail.com>
2025-05-29 11:35:24 -0400

    log

--- src/modelec_com/src/pcb_action_interface.cpp
+++ src/modelec_com/src/pcb_action_interface.cpp
@@ -272,7 +272,7 @@ namespace Modelec
 
     void PCBActionInterface::PCBCallback(const std_msgs::msg::String::SharedPtr msg)
     {
-        RCLCPP_DEBUG(this->get_logger(), "Received message: '%s'", msg->data.c_str());
+        RCLCPP_INFO(this->get_logger(), "Received message: '%s'", msg->data.c_str());
         std::vector<std::string> tokens = split(trim(msg->data), ';');
 
         if (tokens.size() < 2)

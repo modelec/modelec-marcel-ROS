84147b307f301c9eacc529337708cb456cd118c5
acki <ackimixs@protonmail.com>
2025-05-30 10:31:16 -0400

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-MAKEFLAGS="-j1" colcon build --executor sequential --parallel-workers 1 --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j2" colcon build --executor sequential --parallel-workers 1 --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 source install/setup.bash

4d69872306658fb6b9a6ef77323d4bf006c1a621
acki <ackimixs@protonmail.com>
2025-11-19 18:47:11 +0100

    restart node

--- src/modelec_core/launch/modelec.launch.py
+++ src/modelec_core/launch/modelec.launch.py
@@ -109,7 +109,7 @@ def generate_launch_description():
                     executable='pcb_action_interface',
                     name='pcb_action_interface',
                     parameters=[{
-                        'serial_port': "/dev/USB_ACTION",
+                        'serial_port': "/tmp/USB_ACTION",
                         'baudrate': 115200,
                         'name': "pcb_action",
                     }]

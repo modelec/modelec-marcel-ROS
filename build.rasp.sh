a2e190dbc08d5e8abd13087b3a123bc03aa9ead8
Modelec <modelec.isen@gmail.com>
2025-11-26 18:48:08 +0100

    Build in debug

--- build.rasp.sh
+++ build.rasp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-MAKEFLAGS="-j1" colcon build --symlink-install --executor sequential --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON # -DCMAKE_BUILD_TYPE=Debug
+MAKEFLAGS="-j1" colcon build --symlink-install --executor sequential --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
 
 source install/setup.bash

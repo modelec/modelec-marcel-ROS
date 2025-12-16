d0b6bace55c8e42d6b0907ca6fd73486d9ca3c23
acki <ackimixs@protonmail.com>
2025-12-16 21:32:45 +0100

    warning

--- src/modelec_strat/src/action_executor.cpp
+++ src/modelec_strat/src/action_executor.cpp
@@ -26,7 +26,7 @@ namespace Modelec
             });
 
         servo_move_res_sub_ = node_->create_subscription<modelec_interfaces::msg::ActionServoPosArray>(
-            "/action/move/servo/res", 10, [this](const modelec_interfaces::msg::ActionServoPosArray::SharedPtr msg)
+            "/action/move/servo/res", 10, [this](const modelec_interfaces::msg::ActionServoPosArray::SharedPtr)
             {
                 // BUG
                 // if ServoTimed is called this one will trigger so step_running_ will be decremented at the beginning of the Timed one

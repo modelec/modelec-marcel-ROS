e92b128c412ce9bad1f056a7b57d05e8b93e6367
acki <ackimixs@protonmail.com>
2025-12-18 18:22:16 +0100

    servo value

--- src/modelec_strat/src/action/down_action.cpp
+++ src/modelec_strat/src/action/down_action.cpp
@@ -36,24 +36,24 @@ void Modelec::DownAction::Next()
             msg.items.resize(4);
 
             msg.items[0].id = front_ ? 0 : 8;
-            msg.items[0].start_angle = front_ ? 1.49 : 0;
-            msg.items[0].end_angle = front_ ? 0 : 0;
-            msg.items[0].duration_s = 1;
+            msg.items[0].start_angle = front_ ? 1.95 : 0;
+            msg.items[0].end_angle = front_ ? 2.95 : 0;
+            msg.items[0].duration_s = 5;
 
             msg.items[1].id = front_ ? 1 : 9;
-            msg.items[1].start_angle = front_ ? 1.5 : 0;
-            msg.items[1].end_angle = front_ ? 3 : 0;
-            msg.items[1].duration_s = 1;
+            msg.items[1].start_angle = front_ ? 1.9 : 0;
+            msg.items[1].end_angle = front_ ? 0.93 : 0;
+            msg.items[1].duration_s = 5;
 
             msg.items[2].id = front_ ? 2 : 10;
-            msg.items[2].start_angle = front_ ? 3 : 0;
-            msg.items[2].end_angle = front_ ? 1.45 : 0;
-            msg.items[2].duration_s = 1;
+            msg.items[2].start_angle = front_ ? 2.3 : 0;
+            msg.items[2].end_angle = front_ ? 3 : 0;
+            msg.items[2].duration_s = 5;
 
             msg.items[3].id = front_ ? 3 : 11;
-            msg.items[3].start_angle = front_ ? 0 : 0;
-            msg.items[3].end_angle = front_ ? 1.6 : 0;
-            msg.items[3].duration_s = 1;
+            msg.items[3].start_angle = front_ ? 0.8 : 0;
+            msg.items[3].end_angle = front_ ? 0 : 0;
+            msg.items[3].duration_s = 5;
 
             action_executor_->MoveServoTimed(msg);
         }

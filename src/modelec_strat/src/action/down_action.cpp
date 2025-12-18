6e235feeda1e5c811f67913318e5cd2bec6cb8b0
acki <ackimixs@protonmail.com>
2025-12-18 20:44:26 +0100

    servo value

--- src/modelec_strat/src/action/down_action.cpp
+++ src/modelec_strat/src/action/down_action.cpp
@@ -45,15 +45,15 @@ void Modelec::DownAction::Next()
             msg.items[1].end_angle = front_ ? 1.9 : 0;
             msg.items[1].duration_s = 1;
 
-            msg.items[1].id = front_ ? 2 : 10;
-            msg.items[1].start_angle = front_ ? 0.3 : 0;
-            msg.items[1].end_angle = front_ ? 0 : 0;
-            msg.items[1].duration_s = 1;
+            msg.items[2].id = front_ ? 2 : 10;
+            msg.items[2].start_angle = front_ ? 0.3 : 0;
+            msg.items[2].end_angle = front_ ? 0 : 0;
+            msg.items[2].duration_s = 1;
 
-            msg.items[1].id = front_ ? 3 : 11;
-            msg.items[1].start_angle = front_ ? 2.7 : 0;
-            msg.items[1].end_angle = front_ ? 3 : 0;
-            msg.items[1].duration_s = 1;
+            msg.items[3].id = front_ ? 3 : 11;
+            msg.items[3].start_angle = front_ ? 2.7 : 0;
+            msg.items[3].end_angle = front_ ? 3 : 0;
+            msg.items[3].duration_s = 1;
 
             action_executor_->MoveServoTimed(msg);
         }

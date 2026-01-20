d4905f3eb12ebe134a65be705ea9b63fd02d84f0
Modelec <modelec.isen@gmail.com>
2026-01-20 18:28:03 +0100

    change to real serial port

--- src/modelec_core/launch/modelec.launch.py
+++ src/modelec_core/launch/modelec.launch.py
@@ -99,7 +99,7 @@ def generate_launch_description():
                     executable='pcb_odo_interface',
                     name='pcb_odo_interface',
                     parameters=[{
-                        'serial_port': "/tmp/USB_ODO",
+                        'serial_port': "/dev/USB_ODO",
                         'baudrate': 115200,
                         'name': "pcb_odo",
                     }]
@@ -109,7 +109,7 @@ def generate_launch_description():
                     executable='pcb_action_interface',
                     name='pcb_action_interface',
                     parameters=[{
-                        'serial_port': "/tmp/USB_ACTION",
+                        'serial_port': "/dev/USB_ACTION",
                         'baudrate': 115200,
                         'name': "pcb_action",
                     }]

b50a735f609b63d4cd5fdf63a01470d4da360be5
acki <ackimixs@protonmail.com>
2026-02-04 19:28:34 +0100

    not enough internet for fastdds

--- start_ros2.sh
+++ start_ros2.sh
@@ -5,8 +5,8 @@ source /opt/ros/jazzy/setup.bash
 source /home/modelec/Modelec-ROS2/install/setup.bash
 
 export RCL_LOG_LEVEL=info
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+#export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/Modelec-ROS2/fastdds_setup.xml
-export ROS_DOMAIN_ID=128
+#export ROS_DOMAIN_ID=128
 
 exec ros2 launch modelec_core modelec.launch.py "$@"

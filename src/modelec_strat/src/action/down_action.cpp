1a999a32b105b5d3dc37c99d30b674453ffc9481
acki <ackimixs@protonmail.com>
2026-02-09 20:52:04 +0100

    angle value

--- src/modelec_strat/src/action/down_action.cpp
+++ src/modelec_strat/src/action/down_action.cpp
@@ -47,12 +47,12 @@ void Modelec::DownAction::Next()
                 msg.items[2].id = 2;
                 msg.items[2].start_angle = 0.5;
                 msg.items[2].end_angle = inverted_ ? 0 : 3.05;
-                msg.items[2].duration_s = 1.7;
+                msg.items[2].duration_s = 1.3;
 
                 msg.items[3].id = 3;
                 msg.items[3].start_angle = 2.6;
                 msg.items[3].end_angle = inverted_ ? 3.4 : 0.3;
-                msg.items[3].duration_s = 1.7;
+                msg.items[3].duration_s = 1.3;
             }
 
             if (side_ == BACK || side_ == BOTH)

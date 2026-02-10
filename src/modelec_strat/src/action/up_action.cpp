09f25d1ac37103b2d71c546fc21c3b48541555ad
acki <ackimixs@protonmail.com>
2026-02-10 18:32:02 +0100

    value

--- src/modelec_strat/src/action/up_action.cpp
+++ src/modelec_strat/src/action/up_action.cpp
@@ -43,7 +43,7 @@ void Modelec::UPAction::Next()
                 msg.items[1].end_angle = 2.2;
                 msg.items[1].duration_s = 2;
 
-                msg.items[2].id = 2;
+                /*msg.items[2].id = 2;
                 msg.items[2].start_angle = action_executor_->arm_pos_[FRONT].rotated ? 0 : 3.05;
                 msg.items[2].end_angle = 0.5;
                 msg.items[2].duration_s = 2.8;
@@ -51,30 +51,50 @@ void Modelec::UPAction::Next()
                 msg.items[3].id = 3;
                 msg.items[3].start_angle = action_executor_->arm_pos_[FRONT].rotated ? 3.4 : 0.3;
                 msg.items[3].end_angle = 2.6;
-                msg.items[3].duration_s = 2.8;
+                msg.items[3].duration_s = 2.8;*/
 
-                /*if (action_executor_->arm_pos_[FRONT].rotated)
+
+                if (action_executor_->arm_pos_[FRONT].rotated)
                 {
+                    msg.items[2].id = 2;
+                    msg.items[2].start_angle = 0;
+                    msg.items[2].end_angle = 0.5;
+                    msg.items[2].duration_s = 2.8;
+
                     msg.items[3].id = 3;
                     msg.items[3].start_angle = 3.4;
                     msg.items[3].end_angle = 2.6;
                     msg.items[3].duration_s = 2.8;
                 } else
                 {
+                    msg.items[2].id = 2;
+                    msg.items[2].start_angle = 3.05;
+                    msg.items[2].end_angle = 2.35;
+                    msg.items[2].duration_s = 0.5;
+
                     msg.items[3].id = 3;
                     msg.items[3].start_angle = 0.3;
-                    msg.items[3].end_angle = 0.9;
+                    msg.items[3].end_angle = 1;
                     msg.items[3].duration_s = 0.5;
 
-                    auto last_msg = modelec_interfaces::msg::ActionServoTimed();
-                    last_msg.id = 3;
-                    last_msg.start_angle = 0.9;
-                    last_msg.end_angle = 2.6;
-                    last_msg.duration_s = 1;
-                    last_msg.delay_s = 0.5;
+                    auto last_msg_3 = modelec_interfaces::msg::ActionServoTimed();
+                    last_msg_3.id = 3;
+                    last_msg_3.start_angle = 1;
+                    last_msg_3.end_angle = 2.6;
+                    last_msg_3.duration_s = 1;
+                    last_msg_3.delay_s = 0.5;
 
-                    msg.items.push_back(last_msg);
-                }*/
+                    msg.items.push_back(last_msg_3);
+
+                    auto last_msg_2 = modelec_interfaces::msg::ActionServoTimed();
+                    last_msg_2.id = 2;
+                    last_msg_2.start_angle = 2.35;
+                    last_msg_2.end_angle = 0.5;
+                    last_msg_2.duration_s = 1;
+                    last_msg_2.delay_s = 0.5;
+
+                    msg.items.push_back(last_msg_2);
+                }
             }
 
             if (side_ == BACK || side_ == BOTH) {

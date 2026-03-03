9f79db4f357c12f2db4c43570b0537991dafb73f
acki <ackimixs@protonmail.com>
2026-03-03 18:38:20 +0100

    start

--- start_ros2.sh
+++ start_ros2.sh
@@ -6,7 +6,7 @@ source /home/modelec/Modelec-ROS2/install/setup.bash
 
 export RCL_LOG_LEVEL=info
 #export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/Modelec-ROS2/fastdds_setup.xml
+#export FASTRTPS_DEFAULT_PROFILES_FILE=/home/modelec/Modelec-ROS2/fastdds_setup.xml
 #export ROS_DOMAIN_ID=128
 
 exec ros2 launch modelec_core modelec.launch.py "$@"

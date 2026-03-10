de29fac9376bd975e77df951e0d570f7f7c11d22
acki <ackimixs@protonmail.com>
2026-03-10 17:53:04 +0100

    cam

--- src/modelec_com/src/color_detector.cpp
+++ src/modelec_com/src/color_detector.cpp
@@ -78,6 +78,7 @@ namespace Modelec
 
     bool ColorDetector::processSnapshot(std::vector<std::string>& colors, std::string& error)
     {
+        RCLCPP_INFO(get_logger(), "Capturing snapshot from camera at %s", link_.c_str());
         // cv::VideoCapture cap(link_, cv::CAP_V4L2);
         cv::VideoCapture cap(
             "libcamerasrc ! video/x-raw,width=1280,height=720 ! videoconvert ! appsink",

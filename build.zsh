1f02325c9d740a82a717b731ce8b627a2e9f407d
acki <ackimixs@protonmail.com>
2026-03-16 19:42:15 +0100

    pid

--- build.zsh
+++ build.zsh
@@ -1,3 +1,7 @@
+#!/bin/zsh
+source /opt/ros/jazzy/setup.zsh
+source install/setup.zsh
+
 colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
 
 source "install/setup.zsh"

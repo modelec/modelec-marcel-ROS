8373dfb0aee2a23fec49c594e40dec5ea1235528
acki <ackimixs@protonmail.com>
2026-03-16 19:57:55 +0100

    log

--- src/modelec_com/src/pcb_odo_interface.cpp
+++ src/modelec_com/src/pcb_odo_interface.cpp
@@ -122,9 +122,9 @@ namespace Modelec
 
     void PCBOdoInterface::read(const std::string& msg)
     {
-        RCLCPP_INFO(this->get_logger(), "Received from PCB: %s", trim(msg).c_str());
-        // RCLCPP_INFO_ONCE(this->get_logger(), "Received from PCB: %s", trim(msg).c_str());
-        // RCLCPP_DEBUG_SKIPFIRST(this->get_logger(), "Received from PCB: %s", msg.c_str());
+        // RCLCPP_INFO(this->get_logger(), "Received from PCB: %s", trim(msg).c_str());
+        RCLCPP_INFO_ONCE(this->get_logger(), "Received from PCB: %s", trim(msg).c_str());
+        RCLCPP_DEBUG_SKIPFIRST(this->get_logger(), "Received from PCB: %s", msg.c_str());
         std::vector<std::string> tokens = split(trim(msg), ';');
         if (tokens.size() < 2)
         {
@@ -287,9 +287,9 @@ namespace Modelec
     {
         if (IsOk())
         {
-            RCLCPP_INFO(this->get_logger(), "Sending to PCB: %s", trim(data).c_str());
-            // RCLCPP_INFO_ONCE(this->get_logger(), "Sending to PCB: %s", trim(data).c_str());
-            // RCLCPP_DEBUG_SKIPFIRST(this->get_logger(), "Sending to PCB: %s", data.c_str());
+            // RCLCPP_INFO(this->get_logger(), "Sending to PCB: %s", trim(data).c_str());
+            RCLCPP_INFO_ONCE(this->get_logger(), "Sending to PCB: %s", trim(data).c_str());
+            RCLCPP_DEBUG_SKIPFIRST(this->get_logger(), "Sending to PCB: %s", data.c_str());
             this->write(data);
         }
     }
